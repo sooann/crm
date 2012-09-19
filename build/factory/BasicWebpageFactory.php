6a009c97bddaa6cbda3dda265b371024410824fc
Tan Soo Ann <tan_soo_ann@hotmail.com>
2012-09-19 20:13:22 +0800

    separate twig template code from basicwebpagefactory create new factory for twig component in twigfactory.php

--- build/factory/BasicWebpageFactory.php
+++ build/factory/BasicWebpageFactory.php
@@ -1,33 +1,42 @@
 <?php
-
-require_once "vendor/twig/Autoloader.php";
 	
 class BasicWebpageFactory extends BaseFactory {
+	
+	private $html = "";
+	
+	public function getHtml() {
+		return $this->html;
+	}
+	
+	public function setHtml($v) {
+		$this->html = $v;
+	}
+	
+	public function __construct($webpage=null, $html=null) {
+		
+		if ($html==null) {
+			$html=$webpage->getHtml();
+		}
+		
+		$this->setHtml(self::buildwebsite($webpage,$html));
+		
+		$this->setHtml(TwigFactory::run($webpage,$this->getHtml()));
+
+	}
 			
 	public static function run($id, $controller, $html=null) {
 		$query = new SysWebpageQuery();
 		if ($webpage = $query->findPK($id)) {
-			if ($html==null) {
-				$html=$webpage->getHtml();
-			}
-			$document = self::buildwebsite($webpage,$html);
 			
-			//finalise html via template engine
-			Twig_Autoloader::register();
-
-			$loader = new Twig_Loader_String();
-			$twig = new Twig_Environment($loader);
-			$twig->addFilter('var_dump', new Twig_Filter_Function('var_dump'));
-			$document = $twig->render($document,array("params"=>$webpage->toArray()));
-			
-			self::writeHtml($document);
+			$obj = new BasicWebpageFactory ($webpage, $html);
+			self::writeHtml($obj->getHtml());
 			return true;
 			
 		} else {
-			return false;
+			return false;	
 		}
 	}
-	
+
 	private static function buildwebsite($webpage, $html) {
 		if ($webpage->getWebtemplateid()!=null) {
 			$document = self::runTemplate($webpage->getWebtemplateid(), $html);
