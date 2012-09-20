9d31ab8b3555de245ee8cae4884f05aaa1175b20
Tan Soo Ann <tan_soo_ann@hotmail.com>
2012-09-20 11:24:14 +0800

    updates from database code generation tool

--- build/element/element.amyeditor.php
+++ build/element/element.amyeditor.php
@@ -5,26 +5,25 @@
  *
  * 
  *
- * @name	textarea
+ * @name	amyeditor
  * @type	CLASS
  *
 */
 		
 class amyeditor extends element {
-	
-	var $theme='concept';
-	var $bundle = 'php';
-	var $language = 'php';
-	
+var $theme='concept';
+var $bundle = 'php';
+var $language = 'php';
+
 	public function getHTML() {
 		return 
-		      '<textarea ' . 
-		          'id="' . $this->_getHTMLId() . '" ' . 
-		          'name="' . $this->name . '" ' .
-		          $this->html .' -amy-enabled="true" >' .
-		        htmlspecialchars( $this->value ) . 
-		      '</textarea>
-			   <script type="text/javascript" src="/crm/vendor/amyeditor/api/embed/amy.php?theme='.$this->theme.'&bundle='.$this->bundle.'&language='.$this->language.'"></script>';
+				      '<textarea ' . 
+				          'id="' . $this->_getHTMLId() . '" ' . 
+				          'name="' . $this->name . '" ' .
+				          $this->html .' -amy-enabled="true" >' .
+				        htmlspecialchars( $this->value ) . 
+				      '</textarea>
+					   <script type="text/javascript" src="/crm/vendor/amyeditor/api/embed/amy.php?theme='.$this->theme.'&bundle='.$this->bundle.'&language='.$this->language.'"></script>';
 	}
 
 }
