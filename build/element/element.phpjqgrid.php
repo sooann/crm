1a67f2cb46956dd6298b7bf16daa8e401b96232b
Tan Soo Ann <tan_soo_ann@hotmail.com>
2012-09-22 15:00:01 +0800

    updated using database code generation

--- build/element/element.phpjqgrid.php
+++ build/element/element.phpjqgrid.php
@@ -1,20 +1,17 @@
 
 <?php
-	
-require_once 'vendor/jqgrid/jq-config.php';
-	
+	require_once 'vendor/jqgrid/jq-config.php';
 /**
  *
  * 
  *
- * @name	jstree
+ * @name	phpjqgrid
  * @type	CLASS
  *
 */
 		
 class phpjqgrid extends element {
-	
-	var $height= "100%";
+var $height= "100%";
 	var $width = "100%";
 	var $dataType = 'local';
 	var $sql = "";
@@ -24,46 +21,42 @@ class phpjqgrid extends element {
 	var $model = array();
 
 	public function getHTML() {
-		
 		// Connection to the server
-		$conn = new PDO(DB_DSN,DB_USER,DB_PASSWORD);
-		// Tell the db that we use utf-8
-		$conn->query("SET NAMES utf8");
-		// Create the jqGrid instance
-		$grid = new JQGridFactory($conn);
-
-		// Set output format to json
-		$grid->dataType = $this->dataType;
+				$conn = new PDO(DB_DSN,DB_USER,DB_PASSWORD);
+				// Tell the db that we use utf-8
+				$conn->query("SET NAMES utf8");
+				// Create the jqGrid instance
+				$grid = new JQGridFactory($conn);
 		
-		//manually creating model
-		
-		//predefault setting array 
-		$gridoptionsarr = array(
-			"width" => $this->width,
-			"height" => $this->height,
-			"hoverrows" => true);
-		$grid->setGridOptions($gridoptionsarr);
-		
-		//get data from database
-		
-		// Enjoy
-		$this->html = $grid->buildHtml("#grid",null,true, null, null, true,true);
-		$conn = null;
-		
-		return ($this->html);
-		
+				// Set output format to json
+				$grid->dataType = $this->dataType;
+				
+				//manually creating model
+				
+				//predefault setting array 
+				$gridoptionsarr = array(
+					"width" => $this->width,
+					"height" => $this->height,
+					"hoverrows" => true);
+				$grid->setGridOptions($gridoptionsarr);
+				
+				//get data from database
+				
+				// Enjoy
+				$this->html = $grid->buildHtml("#grid",null,true, null, null, true,true);
+				$conn = null;
+				
+				return ($this->html);
 	}
-	
-	public function getModel() {
+
+public function getModel() {
 		
 		//translating model from database to array
 		
 		
 		//return model array
 		return array();
-	}
-
-}
+	}}
 
 ?>
 		
