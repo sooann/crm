7d29f8d99e7baf1c04886486feb6a2a5904118b7
Tan Soo Ann <tan_soo_ann@hotmail.com>
2012-09-27 10:07:26 +0800

    test script for propel

--- testpropel.php
+++ testpropel.php
@@ -1,5 +1,21 @@
 <?php
 
+global $strDBRegion;
+
+$strDBRegion = "SGP";
+
+//Propel DAO init
+require_once 'vendor/propel/runtime/lib/Propel.php';
+Propel::init("build/conf/CRM".$strDBRegion."-conf.php");
+set_include_path("build/classes" . PATH_SEPARATOR . get_include_path());
+
+$webpage = SysWebpageQuery::create()->findPk(4);
+
+var_dump($webpage->getSysWebpagecolumns());
+echo "<br /><br />";
+
+
+$webpage->setSysWebpagecolumns();
 
 
 ?>
