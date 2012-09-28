bc41026efe1214f20dbf1f104c36bd3222ff0086
Tan Soo Ann <tan_soo_ann@hotmail.com>
2012-09-28 15:25:02 +0800

    consolidating onselectrow javascript code to main class

--- build/factory/JQGridFactory.php
+++ build/factory/JQGridFactory.php
@@ -9,6 +9,27 @@ require_once ABSPATH."php/jqGridPdo.php";
 	
 class JQGridFactory extends jqGridRender {
 	
+	protected $gridid;
+	protected $htmlid;
+	protected $onselrow;
+	
+	public function setHtmlId ($v) {
+		$this->htmlid = $v;
+		$this->gridid = $this->htmlid."_grid";
+	}
+	
+	public function setGridId($v) {
+		$this->gridid = $v;
+	}
+	
+	public function getGridId() {
+		return $this->gridid;
+	}
+	
+	public function getHtmlId() {
+		return $this->htmlid;
+	}
+	
 	public $html = '
 		{% autoescape false %}
 			{{ calldecl ("jqgrid.css") }}
@@ -25,29 +46,27 @@ class JQGridFactory extends jqGridRender {
 			
 	}
 	
-	protected $onselrow = <<< ONSELROW
+	public function getOnSelectRow() {
+		$this->onselrow = "
 function(rowid, selected)
 {
 	if(rowid && rowid !== lastSelection) {
-		$("#$gridid").jqGrid('saveRow',lastSelection,{
+		$('#".$this->gridid."').jqGrid('saveRow',lastSelection,{
 				url:'clientArray',
 				aftersavefunc: function() {
-					$("#$htmlid").val(JSON.stringify($('#$gridid').jqGrid('getRowData')));
+					$('#".$this->htmlid."').val(JSON.stringify($('#".$this->gridid."').jqGrid('getRowData')));
 				}
 			});
-		$("#$gridid").jqGrid('restoreRow', lastSelection);
-		$("#$gridid").jqGrid('editRow', rowid,{
+		$('#".$this->gridid."').jqGrid('restoreRow', lastSelection);
+		$('#".$this->gridid."').jqGrid('editRow', rowid,{
 				keys:true,
 				aftersavefunc: function() {
-					$("#$htmlid").val(JSON.stringify($('#$gridid').jqGrid('getRowData')));
+					$('#".$this->htmlid."').val(JSON.stringify($('#".$this->gridid."').jqGrid('getRowData')));
 				}
 			});
 		lastSelection = rowid;
 	}
-}
-ONSELROW;
-	
-	public function getOnSelectRow() {
+}";
 		return $this->onselrow;
 	}
 	
