d3c53bb67817ab4e9afb6ba8b86541b9f80b8318
Derek Tan <tan_soo_ann@hotmail.com>
2013-03-10 22:26:30 +0800

    init configure slim framework

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
 <?php
 
-//include_once "global.php";
+	//include_once "global.php";
 
-$_SESSION["intUserURN"]=NULL;
+	$_SESSION["intUserURN"]=NULL;
 
-$router = new RouterFactory();
-$router->run();
+	//init slim framework
+	require 'Slim/Slim.php';
+	\Slim\Slim::registerAutoloader();
+
+	//setting global variable for slim framework
+
+	$app = new \Slim\Slim();
+	
+	$app->get('/hello/:name', function ($name) {
+		echo "Hello, $name";
+	});
+
+	$app->run();
 
 ?>
