8b8a608688cd6dee9095e61c0c99d0c26d602e95
Derek Tan <tan_soo_ann@hotmail.com>
2013-03-14 21:23:40 +0800

    developing connection to doctrine dbal

--- index.php
+++ index.php
@@ -4,12 +4,31 @@
 
 	$_SESSION["intUserURN"]=NULL;
 
+	//init Doctrine2 DBAL
+	use Doctrine\Common\ClassLoader;
+	require 'Doctrine/Common/ClassLoader.php';
+	$classLoader = new ClassLoader('Doctrine', '/');
+	$classLoader.register();
+	
+	$dbConfig = new \Doctrine\DBAL\Configuration();
+	$connectionParams = array(
+    		'dbname' => 'crmsgp',
+    		'user' => 'root',
+		'password' => 'sooann',
+		'host' => 'localhost',
+		'driver' => 'pdo_mysql',
+	);
+	
+	$conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $dbConfig);
+
 	//init slim framework
 	require 'Slim/Slim.php';
-	\Slim\Slim::registerAutoloader();
+	
+	//Do not require autoloader due to Doctrine Autoloader 
+	//\Slim\Slim::registerAutoloader();
 
 	//setting global variable for slim framework
-
+	
 	$app = new \Slim\Slim();
 	
 	$app->get('/hello/:name', function ($name) {
